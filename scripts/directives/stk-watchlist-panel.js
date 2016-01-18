2022392cfc2997424a04463d5deedce39c9d690a
Dongjie <dongjielu@wustl.edu>
2016-01-18 13:25:37 -0600

    no message

---
+++ scripts/directives/stk-watchlist-panel.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name stockDogApp.directive:stkWatchlistPanel
+ * @description
+ * # stkWatchlistPanel
+ */
+angular.module('stockDogApp')
+  .directive('stkWatchlistPanel', function ($location, $modal, WatchlistService) {
+    return {
+      templateUrl: 'views/templates/watchlist-panel.html',
+      restrict: 'E',
+      scope: {},
+      link: function ($scope) {
+        $scope.watchlist = {};
+        var addListModal = $modal({
+        	scope: $scope,
+        	template: 'views/templates/addlist-modal.html',
+        	show: false
+        });
+
+        $scope.watchlists = WatchlistService.query();
+
+        $scope.
+      }
+    };
+  });
