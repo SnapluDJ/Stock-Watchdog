6be367585a0f559f2864b8e81cdf0119d99b593a
Dongjie <dongjielu@wustl.edu>
2016-01-26 20:25:23 -0600

    no message

--- scripts/services/watchlist-service.js
+++ scripts/services/watchlist-service.js
@@ -18,6 +18,13 @@ angular.module('stockDogApp')
     			parseInt(localStorage['StockDog.nextId']) : 0
     	};
 
+        _each(model.watchlists, function (watchlist) {
+            _.extend(watchlist, WatchlistModel);
+            _.each(watchlist.stocks, function (stock) {
+                _.extend(stock, StockModel);
+            });
+        });
+
     	return model;
     };
 
@@ -46,6 +53,8 @@ angular.module('stockDogApp')
 
     this.save = function (watchlist) {
     	watchlist.id = Model.nextId++;
+        watchlist.stocks = [];
+        _.extend(watchlist, WatchlistModel);
     	Model.watchlists.push(watchlist);
     	saveModel();
     };
@@ -58,6 +67,53 @@ angular.module('stockDogApp')
     	saveModel();
     };
 
+    var StockModel = {
+        save: function () {
+            var watchlist = findById(this.listId);
+            watchlist.recalculate();
+            saveModel();
+        }
+    };
+
+    var WatchlistModel = {
+        addStock: function (stock) {
+            var existingStock = _.find(this.stocks, function (s) {
+                return s.company.symbol === stock.company.symbol;
+            });
+
+            if (existingStock) {
+                existingStock.shares += stock.shares;
+            }else{
+                _.extend(stock, StockModel);
+                this.stocks.push(stock);
+            }
+
+            this.recalculate();
+            saveModel();
+        },
+
+        removeStock: function (stock) {
+            _.remove(this.stocks, function () {
+                return s.company.symbol === stock.company.symbol;
+            });
+
+            this.recalculate();
+            saveModel();
+        },
+
+        recalculate: function () {
+            var calcs = _.reduce(this.stocks, function (calcs, stock) {
+                calcs.shares += stock.shares;
+                calcs.marketValue += stock.marketValue;
+                calcs.dayChange += stock.dayChange;
+                return calcs;
+            }, {shares: 0, marketValue: 0, dayChange: 0});
+
+            this.shares = calcs.shares;
+            this.marketValue = calcs.marketValue;
+            this.dayChange = calcs.dayChange;
+        }
+    };
 
     var Model = loadModel();
   });
